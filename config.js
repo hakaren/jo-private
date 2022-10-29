ea572335a2a8cc1972f550152aebbc52cb4cfbfd
hakaren <115398537+hakaren@users.noreply.github.com>
2022-10-29 21:31:37 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 /**
-   * Create By THE JO BOT
+   * Create By aga
    * Base by Dika Ardnt
 */
 
@@ -17,14 +17,14 @@ global.APIKeys = {
 }
 
 
-global.ownername = 'THE DANIXD'
+global.ownername = 'aga'
 global.autorecording = false //status auto merekam ( auto record )
 global.autoketik = false //status auto mengetik (auto typing)
 global.available = false //status online (online)
 // Other
-global.owner = ['6281251549899','6281251549899','6281251549899']
-global.premium = ['6281251549899']
-global.packname = 'The Jo Bot'
+global.owner = ['6282144607416','6282144607416','6282144607416']
+global.premium = ['6282144607416']
+global.packname = 'je ga Bot'
 global.author = 'WhatsApp Bot'
 global.wlcm = []
 global.sessionName = 'session'
